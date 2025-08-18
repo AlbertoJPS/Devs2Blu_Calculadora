67a4ba6f61f6ad41fb83187cb46d7d4df77d3ff9
kauedotend <kaue.end@gmail.com>
2025-08-18 19:33:17 -0300

    Commit Task 2

---
+++ Calculadora_Team3/CalculadoraNormal.cs
@@ -0,0 +1,32 @@
+﻿using Calculadora_Team3.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_Team3
+{
+    internal class CalculadoraNormal : ICalculator
+    {
+        public double Add(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Divide(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return (a * b);
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return (a - b);
+        }
+    }
+}
