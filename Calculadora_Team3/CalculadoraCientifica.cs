285a5ee0c60a96ed6da1a8e6a5df8e9b20d2f821
kauedotend <kaue.end@gmail.com>
2025-08-18 19:46:17 -0300

    Task 6

---
+++ Calculadora_Team3/CalculadoraCientifica.cs
@@ -0,0 +1,57 @@
+﻿using Calculadora_Team3.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_Team3
+{
+    internal class CalculadoraCientifica : ICalculadoraCientifica
+    {
+        public double Add(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Coseno(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Divide(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Logaritmo(double a)
+        {
+            return Math.Log10((double)a);
+        }
+
+        public double Multiply(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Potência(double a, double b)
+        {
+            return Math.Pow(a, b); //a elevado a potência de b
+        }
+
+        public double Seno(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Subtract(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Tangente(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
