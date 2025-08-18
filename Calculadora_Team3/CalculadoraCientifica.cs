012b5d02906b38e35a2da01755303416feca194e
JulioMatz <jcmatzlima@gmail.com>
2025-08-18 19:52:04 -0300

    Task 5 - Finalizada

---
+++ Calculadora_Team3/CalculadoraCientifica.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Calculadora_Team3.Interfaces;
+
+namespace Calculadora_Team3
+{
+    public class CalculadoraCientifica : Calcular, ICalcularCientifica
+    {
+        public double Seno(double angle)
+        {
+            return Math.Sin(angle);
+        }
+        public double Coseno(double angle)
+        {
+            return Math.Cos(angle);
+        }
+        public double Tangente(double angle)
+        {
+            return Math.Tan(angle);
+        }
+    }
+}
